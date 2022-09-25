ed1969e2d06a3f520380ce47397b85061ba2e75e
walid <walid.nakbi91@gmail.com>
2022-09-25 16:22:55 +0100

    first commit

---
+++ MyForcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyForcast
+//
+//  Created by walid on 25/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
